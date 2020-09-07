413c590644116ba630a8de6ad801f384eab0becf
Jaime <j_diniz4@hotmail.com>
2020-09-07 12:07:52 +0200

    .gitignore is now working

--- backend/api/build/api/v0/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var router = express_1.default.Router();
-router.get("/", function (req, res) { return res.json({ message: "Docker is easy 🐳" }); });
-module.exports = router;
