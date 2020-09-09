f11ebc1ef19731d05214ed940d7d94bf4fda06c2
Jaime <j_diniz4@hotmail.com>
2020-09-10 00:25:56 +0200

    fix: Docker network

--- backend/index.ts
+++ backend/index.ts
@@ -2,19 +2,9 @@ import express, { Application, Request, Response, NextFunction } from "express"
 import bodyParser from "body-parser"
 import cors from "cors";
 
-const mysql = require('mysql2')
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "0.0.0.0"
-const DB = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'books'
-});
-
-DB.connect();
 
 app.listen(PORT, HOST, () => console.log(`App listening on http://localhost:${PORT}/api/v0`));
 
@@ -23,14 +13,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/books', (req, res) => {
-    DB.query(
-        'SELECT * FROM authors;',
-        function (error: any, results: any, fields: any) {
-            console.log(results); // results contains rows returned by server
-            res.send(results);
-            //console.log(fields); // fields contains extra meta data about results, if available
-        }
-    );
+    // DB.query(
+    //     'SELECT * FROM authors;',
+    //     function (error: any, results: any, fields: any) {
+    //         console.log(results); // results contains rows returned by server
+    //         res.send(results);
+    //         //console.log(fields); // fields contains extra meta data about results, if available
+    //     }
+    // );
 });
 
 app.get('/', (req, res) => {
