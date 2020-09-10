656e8f08ef0b97f159a76f63d2de2baa17ebdbdb
Jaime <j_diniz4@hotmail.com>
2020-09-10 15:21:15 +0200

    feat: Add get async reponse

--- backend/api/v0/controllers/database.ts
+++ backend/api/v0/controllers/database.ts
@@ -1,111 +1,214 @@
-import { Request, Response } from 'express'
-import * as log from '../utils/logger';
-import { Client } from 'pg';
+import { Request, Response } from "express";
+import * as log from "../utils/logger";
+import { Pool } from "pg";
 
-const section = 'Controllers | Database |'
+const section = "Controllers | Database |";
 
-const DB = process.env.DB || 'postgres';
-const DB_USER = process.env.DB_USER || 'database-user';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
+const DB = process.env.DB || "postgres";
+const DB_USER = process.env.DB_USER || "database-user";
+const DB_PASSWORD = process.env.DB_PASSWORD || "password";
 
-const client = new Client(`postgres://${DB_USER}:${DB_PASSWORD}@db:5432/${DB}`);
-client.connect(error => {
-    if (error) {
-        log.error({ component: section, message: error.stack!, error });
-    } else {
-        log.info({ component: section, message: 'Connected to Database' });
-    }
-});
+const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@db:5432`;
 
 export async function createTable(req: Request, res: Response) {
-    const component = log.component(`${section} createTable`)
-    client.query(`CREATE TABLE ${req.params.table} (id SERIAL PRIMARY KEY, name text, age integer);`,
-        (error, result) => {
-            if (error) {
-                log.error({ component, message: error.message.replace(/\"/g, "'"), error });
-                return res.status(401).send({
-                    error: true,
-                    message: error.message.replace(/\"/g, "'"),
-                    object: {}
-                });
-            }
-            const message = `table '${req.params.table}' was created.`
-            log.info({ component, message });
-            res.status(201).send({
-                error: false,
-                message,
-                object: {}
-            });
-        })
+  const component = log.component(`${section} createTable`);
+  const pool = new Pool({
+    connectionString: `${connectionString}/${req.params.DB}`,
+  });
+  const client = await pool.connect();
+
+  await (async () => {
+    var response;
+    try {
+      response = await client.query(
+        `CREATE TABLE ${req.params.table} (id SERIAL PRIMARY KEY, name text, age integer);`
+      );
+    } finally {
+      client.release();
+    }
+
+    if (false) {
+      return res.status(401).send({
+        error: true,
+        message: "table not created.",
+        object: {},
+      });
+    }
+
+    const message = `table '${req.params.table}' was created.`;
+    log.info({ component, message });
+    res.status(201).send({
+      error: false,
+      message,
+      object: {},
+    });
+  })().catch((error) => {
+    log.error({ component, message: error.message.replace(/\"/g, "'"), error });
+    return res.status(500).send({
+      error: true,
+      message: error.message.replace(/\"/g, "'"),
+      object: {},
+    });
+  });
 }
 
 export async function deleteTable(req: Request, res: Response) {
-    const component = `${section} deleteTable`
-    client.query(`DROP TABLE ${req.params.table};`,
-        (error, result) => {
-            if (error) {
-                log.error({ component, message: error.message.replace(/\"/g, "'"), error });
-                return res.status(401).send({
-                    error: true,
-                    message: error.message.replace(/\"/g, "'"),
-                    object: {}
-                });
-            }
-            const message = `table '${req.params.table}' was deleted.`
-            log.info({ component, message });
-            res.status(201).send({
-                error: false,
-                message,
-                object: {}
-            });
-        })
+  const component = `${section} deleteTable`;
+  const pool = new Pool({
+    connectionString: `${connectionString}/${req.params.DB}`,
+  });
+  const client = await pool.connect();
+
+  await (async () => {
+    var response;
+    try {
+      response = await client.query(`DROP TABLE ${req.params.table};`);
+    } finally {
+      client.release();
+    }
+
+    if (false) {
+      return res.status(401).send({
+        error: true,
+        message: "table not deleted!",
+        object: {},
+      });
+    }
+
+    const message = `table '${req.params.table}' was deleted.`;
+    log.info({ component, message });
+    res.status(201).send({
+      error: false,
+      message,
+      object: {},
+    });
+  })().catch((error) => {
+    log.error({ component, message: error.message.replace(/\"/g, "'"), error });
+    return res.status(500).send({
+      error: true,
+      message: error.message.replace(/\"/g, "'"),
+      object: {},
+    });
+  });
 }
 
 export async function addRow(req: Request, res: Response) {
-    const component = `${section} addRow`
-    client.query(`INSERT INTO ${req.params.table}(name, age) VALUES ('Jaime', 25) RETURNING *;`,
-        (error, result) => {
-            if (error) {
-                log.error({ component, message: error.message.replace(/\"/g, "'"), error });
-                return res.status(401).send({
-                    error: true,
-                    message: error.message.replace(/\"/g, "'"),
-                    object: {}
-                });
-            }
-            const insertedValue = result?.rows[0]
-            if (insertedValue.name !== 'Jaime'
-                || insertedValue.age != 25) {
-                return res.status(500).send({
-                    error: true,
-                    message: `Error! | Inseted Value: ${insertedValue}`,
-                    object: {}
-                });
-            }
-            res.status(201).send({
-                error: false,
-                message: '',
-                object: insertedValue
-            });
-        })
+  const component = `${section} addRow`;
+  const pool = new Pool({
+    connectionString: `${connectionString}/${req.params.DB}`,
+  });
+  const client = await pool.connect();
+
+  await (async () => {
+    var response;
+    try {
+      response = await client.query(
+        `INSERT INTO ${req.params.table}(name, age) VALUES ('Jaime', 25) RETURNING *;`
+      );
+    } finally {
+      client.release();
+    }
+
+    if (!response?.rows[0]) {
+      return res.status(401).send({
+        error: true,
+        message: "Not inserted!",
+        object: {},
+      });
+    }
+
+    const insertedValue = response?.rows[0];
+    res.status(201).send({
+      error: false,
+      message: "",
+      object: insertedValue,
+    });
+  })().catch((error) => {
+    log.error({ component, message: error.message.replace(/\"/g, "'"), error });
+    return res.status(500).send({
+      error: true,
+      message: error.message.replace(/\"/g, "'"),
+      object: {},
+    });
+  });
 }
 
 export async function getAll(req: Request, res: Response) {
-    const component = `${section} getAll`
-    client.query(`SELECT * FROM ${req.params.table};`,
-        (error, result) => {
-            if (error) {
-                log.error({ component, message: error.message.replace(/\"/g, "'"), error });
-                return res.status(404).send({
-                    error: true,
-                    message: error.message.replace(/\"/g, "'"),
-                    object: {}
-                });
-            }
-            res.status(200).json({
-                error: false,
-                message: '',
-                object: result?.rows
-            });
-        })
+  const component = `${section} getAll`;
+  const pool = new Pool({
+    connectionString: `${connectionString}/${req.params.DB}`,
+  });
+  const client = await pool.connect();
+
+  await (async () => {
+    var response;
+    try {
+      response = await client.query(`SELECT * FROM ${req.params.table};`);
+    } finally {
+      client.release();
+    }
+
+    if (!response?.rows) {
+      return res.status(404).send({
+        error: true,
+        message: "Empty!",
+        object: {},
+      });
+    }
+
+    res.status(200).json({
+      error: false,
+      message: "",
+      object: {
+        rows: response?.rows,
+      },
+    });
+  })().catch((error) => {
+    log.error({ component, message: error.message.replace(/\"/g, "'"), error });
+    return res.status(500).send({
+      error: true,
+      message: error.message.replace(/\"/g, "'"),
+      object: {},
+    });
+  });
+}
+
+export async function getOne(req: Request, res: Response) {
+  const component = `${section} getAll`;
+  const pool = new Pool({
+    connectionString: `${connectionString}/${req.params.DB}`,
+  });
+  const client = await pool.connect();
+
+  await (async () => {
+    var response;
+    try {
+      response = await client.query(
+        `SELECT * FROM ${req.params.table} WHERE id=${req.params.id};`
+      );
+    } finally {
+      client.release();
+    }
+
+    if (!response?.rows[0]) {
+      return res.status(404).send({
+        error: true,
+        message: "Not founded!",
+        object: {},
+      });
+    }
+
+    res.status(200).json({
+      error: false,
+      message: "",
+      object: response?.rows[0],
+    });
+  })().catch((error) => {
+    log.error({ component, message: error.message.replace(/\"/g, "'"), error });
+    return res.status(500).send({
+      error: true,
+      message: error.message.replace(/\"/g, "'"),
+      object: {},
+    });
+  });
 }
--- backend/api/v0/routes/database.ts
+++ backend/api/v0/routes/database.ts
@@ -1,19 +1,45 @@
-import express from "express"
-import { createTable, deleteTable, addRow, getAll } from '../controllers/database'
+import express from "express";
+import {
+  // createDatabase,
+  // deleteDatabase,
+  // updateTable,
+  createTable,
+  deleteTable,
+  addRow,
+  // deleteRow,
+  // updateRow,
+  getAll,
+  getOne,
+} from "../controllers/database";
 
 const router = express.Router();
 
-router.get("/createTable/:table", createTable)
-router.get("/deleteTable/:table", deleteTable)
-router.get("/addRow/:table", addRow)
-router.get("/getAll/:table", getAll)
+// Modify databases
+// router.post('/:DB', createDatabase)
+// router.delete('/:DB', deleteDatabase)
+
+// Modify tables
+router.post("/:DB/table/:table", createTable);
+router.delete("/:DB/table/:table", deleteTable);
+// router.put('/:DB/table/:table', updateTable)
+
+// Modify rows
+router.put("/:DB/table/:table", addRow);
+// router.delete('/:DB/table/:table/:id', deleteRow)
+// router.put('/:DB/table/:table/:id', updateRow)
+
+// Get info
+router.get("/:DB/table/:table", getAll);
+router.get("/:DB/table/:table/:id", getOne);
+
+// Default route
 router.get("/", (req, res) => {
-    const response = {
-        error: false,
-        msg: "DB",
-        data: {},
-    };
-    return res.status(200).json(response);
+  const response = {
+    error: false,
+    msg: "DB",
+    data: {},
+  };
+  return res.status(200).json(response);
 });
 
 module.exports = router;
